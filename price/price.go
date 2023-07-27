f0b419a484f2bfb659e73577922a95993568c69d
Yashwanth Reddy <yashwanth@renlabs.org>
2023-07-28 00:48:07 +0530

    temp patch for testing

--- price/price.go
+++ price/price.go
@@ -1,11 +1,7 @@
 package price
 
 import (
-	"encoding/json"
-	"fmt"
 	"math"
-	"net/http"
-	"time"
 
 	"github.com/susruth/wbtc-garden/config"
 	"github.com/susruth/wbtc-garden/model"
@@ -26,28 +22,29 @@ func NewPriceChecker(store Store, url string) *PriceChecker {
 }
 
 func (p *PriceChecker) Run() error {
-	for {
-		resp, err := http.Get(p.url)
-		if err != nil {
-			fmt.Println("failed to get prices", err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-		defer resp.Body.Close()
+	// for {
+	// 	resp, err := http.Get(p.url)
+	// 	if err != nil {
+	// 		fmt.Println("failed to get prices", err)
+	// 		time.Sleep(5 * time.Second)
+	// 		continue
+	// 	}
+	// 	defer resp.Body.Close()
 
-		var data map[string]map[string]float64
-		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			fmt.Println("failed to decode response", err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
+	// 	var data map[string]map[string]float64
+	// 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+	// 		fmt.Println("failed to decode response", err)
+	// 		time.Sleep(5 * time.Second)
+	// 		continue
+	// 	}
 
-		fmt.Println(data["bitcoin"]["usd"])
-		if err := p.store.SetPrice("bitcoin", "ethereum", float64(data["bitcoin"]["usd"])); err != nil {
-			return err
-		}
-		time.Sleep(10 * time.Second)
-	}
+	// 	fmt.Println(data["bitcoin"]["usd"])
+	// 	if err := p.store.SetPrice("bitcoin", "ethereum", float64(data["bitcoin"]["usd"])); err != nil {
+	// 		return err
+	// 	}
+	// 	time.Sleep(10 * time.Second)
+	// }
+	return p.store.SetPrice("bitcoin", "ethereum", float64(30000))
 }
 
 func GetPrice(asset string, chain model.Chain, amount float64, PriceInUSD float64) float64 {
