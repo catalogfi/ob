aa5422a285522a03a8bc2be87029cf4103114f31
Yashwanth <yashwanth@renlabs.org>
2024-01-27 18:04:43 +0530

    added expiry data

--- store/customlimits.go
+++ store/customlimits.go
@@ -8,11 +8,11 @@ type CustomLimits struct {
 }
 
 var GrantedLimits = map[string]CustomLimits{
-	"0xe103abfa0f867e53cef1ad2cb0dcbc193b385a93": CustomLimits{
+	"0xe103abfa0f867e53cef1ad2cb0dcbc193b385a93": {
 		Amount: 300000000,
 		Expiry: time.Date(2024, time.January, 28, 0, 0, 0, 0, time.UTC),
 	},
-	"0x3cb762058f019c3abcd5e4a07957ee996ee319bd": CustomLimits{
+	"0x3cb762058f019c3abcd5e4a07957ee996ee319bd": {
 		Amount: 300000000,
 		Expiry: time.Date(2024, time.January, 28, 0, 0, 0, 0, time.UTC),
 	},
